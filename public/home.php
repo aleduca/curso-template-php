dadda93a9233f53b22abb7604f4b6207fce6eb44
Alexandre Cardoso <xandecar@hotmail.com>
2022-05-08 15:15:17 -0300

    How Outout Buffer work

---
+++ public/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Home</h2>
+
+    <p>Olá <?php echo $name; ?> você tem <?php echo $age ?> anos.</p>
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo 'Home';
+$data = ['name' => 'Alexandre', 'age' => 40];
+
+ob_start();
+
+extract($data);
+
+require 'home.php';
+
+$content = ob_get_contents();
+
+ob_end_clean();
+
+var_dump($content);
